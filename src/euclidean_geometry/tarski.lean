5676ebe7f82fd736dd30a21d32b21f5e8b7d9b0c
FrederickPu <pufrederick51@gmail.com>
2023-04-10 01:00:19 +0000

    proved some basic theorems

--- src/euclidean_geometry/tarski.lean
+++ src/euclidean_geometry/tarski.lean
@@ -16,7 +16,7 @@ structure tarski_geo (M α : Type)
 (cong_trans (x y z u v w: α) : H.cong x y z u ∧ H.cong x y v w → H.cong z u v w)
 
 (bet_id (x y : α) : H.B x y x → x = y)
-(ax_pasch (x y z u v a : α) : (H.B x u z ∧ H.B y v z) → ∃ a, (H.B y a u ∧ H.B v a x))
+(ax_pasch (x y z u v : α) : (H.B x u z ∧ H.B y v z) → ∃ a, (H.B y a u ∧ H.B v a x))
 (ax_cont (φ ψ : α → Prop): 
 ∃ a : α, (∀ x, ∀y, ((φ x ∧ ψ y) → H.B a x y) →  (∃ b,∀x,∀y,((φ(x) ∧ ψ(y)) → H.B x b y))) 
 )-- axiom of continuity
@@ -27,4 +27,97 @@ structure tarski_geo (M α : Type)
 (ax_eucA (x y z u v w : α): ((H.B x y w ∧  H.cong x y y w) ∧ (H.B x u v ∧ H.cong x u u v) ∧ (H.B y u z ∧  H.cong y u u z)) → H.cong y z v w) -- think of a parrallelagram
 (five_seg (x y z u x' y' z' u' : α) :
 (x ≠ y ∧ H.B x y z ∧ H.B x' y' z' ∧ H.cong x y x' y' ∧ H.cong y z y' z' ∧ H.cong x u x' u' ∧ H.cong y u y' u') → H.cong z u z' u')
-(seg_con (x y a b : α): ∃z, H.B x y z ∧ H.cong x y a b)
+(seg_con (x y a b : α): ∃z, H.B x y z ∧ H.cong y z a b)
+
+-- congruence for triangles
+-- def tarski_geo.cong' {M α : Type} [add_semigroup M] [linear_order M] [densely_ordered M] {T : tarski_geo M α}
+-- (a b c a' b' c' : α) := 
+-- T.H.cong a b a' b' ∧ T.H.cong b c b' c' ∧ T.H.cong a c a' c'
+
+
+
+namespace tarski_geo
+
+def cong' {M α : Type} [add_semigroup M] [linear_order M] [densely_ordered M] 
+(T : tarski_geo M α) (a b c a' b' c' : α) := 
+T.H.cong a b a' b' ∧ T.H.cong b c b' c' ∧ T.H.cong a c a' c'
+
+variables {M α : Type} [add_semigroup M] [linear_order M] [densely_ordered M] {T : tarski_geo M α}
+
+-- proving basic properties of congruency
+
+theorem cong_refl' {x y : α} : T.H.cong x y x y :=
+begin
+apply T.cong_trans y x x y x y,
+split;
+exact T.cong_refl y x,
+end
+
+theorem cong_symm {x y a b : α} : T.H.cong x y a b → T.H.cong a b x y :=
+begin
+intro p,
+apply T.cong_trans x y a b x y,
+split,
+exact p,
+exact cong_refl',
+end
+
+theorem cong_trans' {x y z u v w : α} : T.H.cong x y z u → T.H.cong z u v w→ T.H.cong x y v w :=
+begin
+intros p q,
+apply T.cong_trans z u x y v w,
+split,
+exact cong_symm p,
+exact q,
+end
+
+-- proving basic properties of betweeness
+theorem bet_symm {x y z : α} : T.H.B x y z → T.H.B z y x:=
+begin
+cases T.seg_con y x x x,
+
+end
+theorem bet_refl {x y : α} : T.H.B x x y :=
+begin
+cases T.seg_con x x x x,
+have := T.cong_id x w x h.right,
+rw ← this at h,
+end
+
+theorem exists_midpoint : ∃ (P Q R : α), T.H.B P Q R ∧ T.H.cong P Q Q R :=
+begin
+rcases T.low_dim with ⟨a, ⟨b, ⟨c, h⟩⟩⟩,
+
+-- c ----> a ----> z ----> w
+-- az and zw are both of length bc
+cases T.seg_con c a b c with z h1, 
+cases T.seg_con a z b c with w h2,
+use a, use z, use w,
+
+split,
+  exact h2.left,
+
+  have := cong_symm h2.right,
+  exact cong_trans' h1.right this,
+end
+
+-- exists perpindicular biscetor 
+-- where perpindicular is defined using congruent rather than similar triangles
+theorem exists_perp_bi : ∃ a b c d, cong' T a b d c b d :=
+begin
+simp only [cong'],
+
+rcases T.low_dim with ⟨a, ⟨b, ⟨c, h⟩⟩⟩,
+
+-- c ----> a ----> z ----> w
+-- az and zw are both of length bc
+cases T.seg_con c a b c with c1 hc1, 
+cases T.seg_con a c1 b c with c2 hc2,
+
+cases T.seg_con b a b c with b1 hb1, 
+cases T.seg_con a b1 b c with b2 hb2,
+
+have := T.ax_pasch c2 b2 a c1 b1,
+end
+
+end tarski_geo
