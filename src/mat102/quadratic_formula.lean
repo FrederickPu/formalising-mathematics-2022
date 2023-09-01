a208e9c2fe0e4d424fd6bee818d01717bb126756
FrederickPu <pufrederick51@gmail.com>
2023-09-01 22:02:12 +0000

    started proving quadratic formuulla

---
+++ src/mat102/quadratic_formula.lean
@@ -0,0 +1,24 @@
+import data.real.sqrt
+
+example (a b c : ℝ) (ha : a ≠ 0) (h : b^2 - 4*a*c ≥ 0): ∀ x : ℝ, a*x^2+b*x+c = 0 → x = (-b + (b^2 - 4*a*c).sqrt)/(2*a) ∨ x =  (-b - (b^2 - 4*a*c).sqrt)/(2*a) := begin
+intro x,
+intro h,
+have h : x^2 + b/a*x + c/a = 0,
+{
+  have wow : (a * x ^ 2 + b * x + c)/a = 0, norm_num [ha, h],
+  rw ← wow,
+  field_simp, ring,
+},
+have h :   x^2 + 2 * x * (b / (2 * a)) + (b^2 / (4 * a^2)) - (b^2 / (4 * a^2)) + (c / a) = 0,
+rw ← h, field_simp, ring,
+have h : (x + (b / (2 * a)))^2 = ((b^2) / (4 * (a^2))) - (c / a),
+{
+  suffices : (x + (b / (2 * a)))^2 - (((b^2) / (4 * (a^2))) - (c / a)) = 0,
+  linarith,
+
+  rw [← h],
+  ring, field_simp, ring,
+},
+have h : (x + (b / (2 * a)))^2 = (b^2 - 4 * a * c) / (4 * a^2),
+rw h, field_simp, ring,
+end
