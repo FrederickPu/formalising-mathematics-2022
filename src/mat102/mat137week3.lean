f45d78e1c18699c7f99c4c7f9b5d290a309e3f10
Frederick Pu <frederick.pu@mail.utoronto.ca>
2023-09-19 22:22:40 -0400

    Hi I'm noah im from whipby ontario

--- src/mat102/mat137week3.lean
+++ src/mat102/mat137week3.lean
@@ -31,7 +31,7 @@ rw nat.mul_div_cancel k this,
   exact aliif,
 },
 end 
-example (a b : ℕ) : b ≠ 0 → share_p a b → ∃ a' b' : ℕ, b' < b ∧ (a' : ℚ) / (b' : ℚ) < (a : ℚ) / (b : ℚ) := begin
+example (a b : ℕ) : b > 0 → share_p a b → ∃ a' b' : ℕ, b' < b ∧ (a' : ℚ) / (b' : ℚ) = (a : ℚ) / (b : ℚ) := begin
 intros w h,
 rcases h with ⟨p, ⟨hp, ⟨ha, hb⟩⟩⟩,
 use (a / p), use (b / p),
@@ -40,10 +40,18 @@ have l : p * (b / p) = b, exact nat.mul_div_cancel' hb,
 suffices : b / p < p * (b/p),
 exact eq.trans_gt l this,
 suffices : 1 * (b / p)  < p * (b/p), linarith,
-have : b ≠ 0, exact w,
-have : b / p ≠ 0, exact oops b p w hb,
-suffices : 1 < p,
+have  : b / p > 0, exact oops b p w hb,
+suffices l : 1 < p,
+exact (mul_lt_mul_right this).mpr l,
+have : p > 0, exact nat.pos_of_dvd_of_pos hb w,
+cases nat.succ_le_iff.mpr this,
+exact false.elim (hp (refl 1)),
+exact (ne.symm hp).lt_of_le _x,
 
+have := nat.pos_of_dvd_of_pos hb w,
+have : (p:ℚ) ≠ 0 := nat.cast_ne_zero.mpr (ne_of_gt this),
+rw [rat.coe_nat_div a p ha, rat.coe_nat_div b p hb],
+field_simp,
 end  
 
 example (a b : ℤ) (hb : b ≠ 0): ∃ p q : ℤ, q ≠ 0 ∧ (a : ℚ) / (b : ℚ) = (p : ℚ) / (q : ℚ) := begin
